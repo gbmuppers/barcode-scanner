0ca28404e8b23c2c20cdcc8ab64732f2525487f9
Gibran Ojeda <16161306@itoaxaca.edu.mx>
2021-08-30 10:50:58 -0500

    dist

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
       </table>
     </div>
     <div class="container" style="margin-bottom:1rem;">
-      <Scanner :onDetected="logIt" :readerSize="readerSize" :readerTypes="['code_39_vin_reader','code_39_reader']"></Scanner>
+      <Scanner :onDetected="logIt" :readerSize="readerSize" :readerTypes="['code_128_reader','ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader','i2of5_reader','2of5_reader','code_93_reader']"></Scanner>
     </div>
   </div>
 </template>
